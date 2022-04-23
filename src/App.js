1529ddd3048977eeaacfbb04e51e48aea4fd86b0
remarshal <marshalwaldendev@gmail.com>
2022-04-23 18:00:46 -0400

    initial background and container

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className='container'>
+
       <h1>App</h1>
+      </div>
     </div>
   );
 }
