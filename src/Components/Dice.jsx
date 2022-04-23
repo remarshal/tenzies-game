af4f52c7551800da92ec40891de6c1bb7f65736e
remarshal <marshalwaldendev@gmail.com>
2022-04-23 18:25:34 -0400

    setup dice component

---
+++ src/Components/Dice.jsx
@@ -0,0 +1,15 @@
+import "../style.css"
+
+const Dice = () => {
+    return (
+        <div className="dice--container">
+            <div className="dice">
+                <div className="dice--number">
+                    <h2>1</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dice;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,10 +1,12 @@
 import '../style.css';
+import Dice from './Dice';
 
 function Main() {
     return (
         <div className="app">
             <div className="container">
                 <h1>Tenzies Game</h1>
+                <Dice />
             </div>
         </div>
     )
