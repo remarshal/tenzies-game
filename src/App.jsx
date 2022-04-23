b47c75d8856d96c40415405f176824293bc892ba
remarshal <marshalwaldendev@gmail.com>
2022-04-23 18:10:34 -0400

    moved app elements to Main component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <div>
+        <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,13 @@
+import '../style.css';
+
+function Main() {
+    return (
+        <div className="app">
+            <div className="container">
+                <h1>Tenzies Game</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
