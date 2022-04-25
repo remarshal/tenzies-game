ee3303cdb7b04a0334e7c5c2883fc262cc7b561c
remarshal <marshalwaldendev@gmail.com>
2022-04-25 14:54:45 -0400

    moved main into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import "./style.css";
-import Main from "./Components/Main";
+import Dice from "./Components/Dice";
 
 function App() {
   return (
-    <div>
-        <Main />
-    </div>
+    <main>
+      <h1>Tenzies Game</h1>
+      <div className="container">
+        <div className="dice--container">
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+        </div>
+      </div>
+      <h3>marshal.rocks</h3>
+    </main>
   );
 }
 
