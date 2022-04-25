fcafcf534c1694ac2c9e912944a955542a840832
remarshal <marshalwaldendev@gmail.com>
2022-04-25 15:56:46 -0400

    added state, mapped array to assign random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
+import React, { useState }  from 'react';
 import "./style.css";
 import Dice from "./Components/Dice";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       newDice.push(Math.floor(Math.random() * 6) + 1);
-      
     }
     return newDice;
   }
 
-  console.log(allNewDice());
+  const diceNumbers = dice.map(number => <Dice value={number} />);
 
   return (
     <main>
       <h1>Tenzies Game</h1>
       <div className="container">
         <div className="dice--container">
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
-          <Dice value="1" />
+          {diceNumbers}
         </div>
       </div>
       <h3>marshal.rocks</h3>
